6229d8ad176a820858e66dd83d9c6077d60972f5
artur-kovach <artur-kovach@seznam.cz>
2025-01-08 16:16:39 +0100

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -166,13 +166,18 @@ async def main():
 
     initialize_slots()
 
-    # Запуск webhook
-    await app.run_webhook(
-        listen="0.0.0.0",
-        port=8000,
-        url_path="/webhook",
-        webhook_url="https://blog.keramika.uz.ua/webhook",
-    )
+import os
+
+# Отримуємо порт із змінної середовища або використовуємо 8000 за замовчуванням
+port = int(os.getenv("PORT", 8000))
+
+await app.run_webhook(
+    listen="0.0.0.0",
+    port=port,
+    url_path="/webhook",
+    webhook_url="https://blog.keramika.uz.ua/webhook",
+)
+
 
 if __name__ == "__main__":
     asyncio.run(main())  # Запуск main за допомогою asyncio.run
